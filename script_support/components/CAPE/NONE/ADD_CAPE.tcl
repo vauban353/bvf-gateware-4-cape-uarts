9212a032346349ac73fa60486a2f2f893db518cb
vauban353 <vauban353@gmail.com>
2023-10-11 20:30:31 +0100

    Cape: Add user LEDs to minimal cape.
    This is requrired to help seeing boot and reprogramming progress.

--- script_support/components/CAPE/NONE/ADD_CAPE.tcl
+++ script_support/components/CAPE/NONE/ADD_CAPE.tcl
@@ -35,8 +35,75 @@ sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:CAPE_APB
 #-------------------------------------------------------------------------------
 
 sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_F2M} -value {GND} 
-sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_OE_M2F} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:FIC_1_AXI4_INITIATOR} 
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:FIC_1_AXI4_TARGET} 
 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[27:27]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[26:26]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[25:25]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[24:24]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[23:23]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[22:22]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[21:21]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[20:20]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[19:19]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[18:18]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[17:17]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[16:16]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[15:15]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[14:14]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[13:13]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[12:12]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[11:11]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[10:10]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[9:9]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[8:8]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[7:7]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[6:6]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[5:5]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[4:4]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[3:3]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[2:2]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[1:1]"} 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F} -pin_slices {"[0:0]"} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[14:14]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[12:12]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[13:13]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[22:22]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[27:27]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[26:26]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[25:25]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[24:24]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[23:23]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[15:15]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[21:21]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[20:20]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[19:19]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[18:18]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[17:17]} 
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[16:16]} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[6:6]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[0:0]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[1:1]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[2:2]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[3:3]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[4:4]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[5:5]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[7:7]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[8:8]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[9:9]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[10:10]} -port_name {} 
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {BVF_RISCV_SUBSYSTEM:GPIO_2_M2F[11:11]} -port_name {} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_0} -new_port_name {P8_3_USER_LED_0} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_1} -new_port_name {P8_4_USER_LED_1} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_2} -new_port_name {P8_5_USER_LED_2} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_3} -new_port_name {P8_6_USER_LED_3} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_4} -new_port_name {P8_7_USER_LED_4} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_5} -new_port_name {P8_8_USER_LED_5} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F} -new_port_name {P8_9_USER_LED_6} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_6} -new_port_name {P8_10_USER_LED_7} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_7} -new_port_name {P8_11_USER_LED_8} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_8} -new_port_name {P8_12_USER_LED_9} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_9} -new_port_name {P8_13_USER_LED_10} 
+sd_rename_port -sd_name ${sd_name} -current_port_name {GPIO_2_M2F_10} -new_port_name {P8_14_USER_LED_11} 
---
+++ script_support/components/CAPE/NONE/Readme.md
@@ -0,0 +1,103 @@
+#  Minimal Cape
+
+## P8 Header
+
+| Signal | Control                    | Description |
+|--------|----------------------------|-------------|
+| P8_1   | n/a                        | GND         |
+| P8_2   | n/a                        | GND         |
+| P8_3   | MSS GPIO_2[0]              | User LED 0  |
+| P8_4   | MSS GPIO_2[1]              | User LED 1  |
+| P8_5   | MSS GPIO_2[2]              | User LED 2  |
+| P8_6   | MSS GPIO_2[3]              | User LED 3  |
+| P8_7   | MSS GPIO_2[4]              | User LED 4  |
+| P8_8   | MSS GPIO_2[5]              | User LED 5  |
+| P8_9   | MSS GPIO_2[6]              | User LED 6  |
+| P8_10  | MSS GPIO_2[7]              | User LED 7  |
+| P8_11  | MSS GPIO_2[8]              | User LED 8  |
+| P8_12  | MSS GPIO_2[9]              | User LED 9  |
+| P8_13  | MSS GPIO_2[10]             | User LED 10 |
+| P8_14  | MSS GPIO_2[11]             | User LED 11 |
+| P8_15  | n/a                        | NC          |
+| P8_16  | n/a                        | NC          |
+| P8_17  | n/a                        | NC          |
+| P8_18  | n/a                        | NC          |
+| P8_19  | n/a                        | NC          |
+| P8_20  | n/a                        | NC          |
+| P8_21  | n/a                        | NC          |
+| P8_22  | n/a                        | NC          |
+| P8_23  | n/a                        | NC          |
+| P8_24  | n/a                        | NC          |
+| P8_25  | n/a                        | NC          |
+| P8_26  | n/a                        | NC          |
+| P8_27  | n/a                        | NC          |
+| P8_28  | n/a                        | NC          |
+| P8_29  | n/a                        | NC          |
+| P8_30  | n/a                        | NC          |
+| P8_31  | n/a                        | NC          |
+| P8_32  | n/a                        | NC          |
+| P8_33  | n/a                        | NC          |
+| P8_34  | n/a                        | NC          |
+| P8_35  | n/a                        | NC          |
+| P8_36  | n/a                        | NC          |
+| P8_37  | n/a                        | NC          |
+| P8_38  | n/a                        | NC          |
+| P8_39  | n/a                        | NC          |
+| P8_40  | n/a                        | NC          |
+| P8_41  | n/a                        | NC          |
+| P8_42  | n/a                        | NC          |
+| P8_43  | n/a                        | NC          |
+| P8_44  | n/a                        | NC          |
+| P8_45  | n/a                        | NC          |
+| P8_46  | n/a                        | NC          |
+
+## P9 Header
+
+| Signal | Control                    | Description |
+|--------|----------------------------|-------------|
+| P9_1   | n/a                        | GND         |
+| P9_2   | n/a                        | GND         |
+| P9_3   | n/a                        | VCC 3.3V    |
+| P9_4   | n/a                        | VCC 3.3V    |
+| P9_5   | n/a                        | VDD 5V      |
+| P9_6   | n/a                        | VDD 5V      |
+| P9_7   | n/a                        | SYS 5V      |
+| P9_8   | n/a                        | SYS 5V      |
+| P9_9   | n/a                        | NC          |
+| P9_10  | n/a                        | SYS_RSTN    |
+| P9_11  | n/a                        | NC          |
+| P9_12  | n/a                        | NC          |
+| P9_13  | n/a                        | NC          |
+| P9_14  | n/a                        | NC          |
+| P9_15  | n/a                        | NC          |
+| P9_16  | n/a                        | NC          |
+| P9_17  | n/a                        | NC          |
+| P9_18  | n/a                        | NC          |
+| P9_19  | MSS I2C0                   | I2C0 SCL    |
+| P9_20  | MSS I2C0                   | I2C0 SDA    |
+| P9_21  | n/a                        | NC          |
+| P9_22  | n/a                        | NC          |
+| P9_23  | n/a                        | NC          |
+| P9_24  | n/a                        | NC          |
+| P9_25  | n/a                        | NC          |
+| P9_26  | n/a                        | NC          |
+| P9_27  | n/a                        | NC          |
+| P9_28  | n/a                        | NC          |
+| P9_29  | n/a                        | NC          |
+| P9_30  | n/a                        | NC          |
+| P9_31  | n/a                        | NC          |
+| P9_32  | n/a                        | VDD ADC     |
+| P9_33  | n/a                        | ADC input 4 |
+| P9_34  | n/a                        | AGND        |
+| P9_35  | n/a                        | ADC input 6 |
+| P9_36  | n/a                        | ADC input 5 |
+| P9_37  | n/a                        | ADC input 2 |
+| P9_38  | n/a                        | ADC input 3 |
+| P9_39  | n/a                        | ADC input 0 |
+| P9_40  | n/a                        | ADC input 1 |
+| P9_41  | n/a                        | NC          |
+| P9_42  | n/a                        | NC          |
+| P9_43  | n/a                        | GND         |
+| P9_44  | n/a                        | GND         |
+| P9_45  | n/a                        | GND         |
+| P9_46  | n/a                        | GND         |
