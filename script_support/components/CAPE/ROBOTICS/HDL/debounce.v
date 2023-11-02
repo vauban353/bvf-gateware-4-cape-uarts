ccc0c37c1b71043e6901b8634d5adda4ece940c9
vauban353 <vauban353@gmail.com>
2023-11-02 16:08:05 +0000

    Robotics cape: adjust encoder debounce.

--- script_support/components/CAPE/ROBOTICS/HDL/debounce.v
+++ script_support/components/CAPE/ROBOTICS/HDL/debounce.v
@@ -9,7 +9,7 @@ module debounce
     output reg switch_out
   );
 
-  localparam bounce_limit_upper_bit = 15;
+  localparam bounce_limit_upper_bit = 13;
   reg [bounce_limit_upper_bit:0] bounce_count = 0;
 
   //---------------------------------------------------------------------------
